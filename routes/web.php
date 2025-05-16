8620f8a2a7833c0d84f0e32eefda9adf15fa16e3
ayna-svg <ina07667@gmail.com>
2025-05-16 07:25:48 +0700

    testing saja

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [Dashboard::class, 'index']);
